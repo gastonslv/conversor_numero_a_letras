e67e2da9a7f394e2bbd2fd99e51160f2010a837a
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2023-12-08 20:03:49 -0300

    valores tipo lista y asignacion de los mismos

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from f_cargarDatos import cargarDato
 from f_convertir import convertir
 from f_ordenarDatos import ordenarPosiciones, asignarUnidades
 # Inicializacion variables globales
-unidades = 0
-millares = 0
-millones = 0
+unidades = []
+millares = []
+millones = []
 
 numero = 0
 palabra_final = ''
@@ -13,9 +13,6 @@ decimal = 0
 
 # MAIN
 numero = cargarDato()
-asignarUnidades(numero)
+unidades, millares, millones = asignarUnidades(numero)
 
-"""
-palabra_final = convertir(numero)
-print(palabra_final.capitalize())
-"""
+print(millones, millares, unidades)
